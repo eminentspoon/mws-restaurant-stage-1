ae022235fbdbba9365d1af613d08422176a40245
eminentspoon <contact@eminentspoon.com>
2018-03-28 20:41:25 +0100

    fix: Add alt text to restaurant images

--- js/main.js
+++ js/main.js
@@ -154,6 +154,7 @@ createRestaurantHTML = restaurant => {
   const image = document.createElement("img");
   image.className = "restaurant-img";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} - ${restaurant.neighborhood}`;
   li.append(image);
 
   const name = document.createElement("h1");
