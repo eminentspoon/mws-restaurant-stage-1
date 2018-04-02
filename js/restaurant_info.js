2ed7db12ffbc22646fb52accb8096a3e1467a5fc
eminentspoon <contact@eminentspoon.com>
2018-04-02 17:02:34 +0100

    fix: Make breadcrumb more accessible
    - Add appropriate aria attributes to breadcrumb container and children
    - Wrap in nav landmark to make more semantic
    - Change from UL to OL to be more semantic
    - Correct styles to reflect changes in markup

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -205,7 +205,8 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById("breadcrumb");
   const li = document.createElement("li");
   li.innerHTML = restaurant.name;
-  breadcrumb.appendChild(li);
+  li.setAttribute("aria-current", "page");
+  breadcrumb.querySelector("ol").appendChild(li);
 };
 
 /**
