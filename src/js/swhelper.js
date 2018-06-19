08f6114c8a9f2c1874a323de74cfa131a3995885
eminentspoon <contact@eminentspoon.com>
2018-06-19 21:38:24 +0100

    chore: Change to use minified css / js

--- src/js/swhelper.js
+++ src/js/swhelper.js
@@ -1,7 +1,7 @@
 class SWHelper {
   static registerServiceWorker() {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js");
+      navigator.serviceWorker.register("/sw.min.js");
     }
   }
 }
