3e3a39f71c623f4e0b7e1fb15410074eb15c32cf
eminentspoon <contact@eminentspoon.com>
2018-07-28 14:43:38 +0100

    feat: Reorder file to make more sense

--- src/sw.js
+++ src/sw.js
@@ -37,6 +37,225 @@ const reviewStore = "reviews";
 const unsubmittedReviewStore = "reviews-pending";
 const unsubmittedFavouriteStatus = "favourite-pending";
 
+self.addEventListener("install", event => {
+  event.waitUntil(cacheBaseAssets());
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(cleanOldCaches());
+  event.waitUntil(initStore());
+});
+
+self.addEventListener("fetch", event => {
+  const urlString = event.request.url;
+
+  // Handle calls to restaurant api
+  if (urlString.startsWith(restaurantApiUrl)) {
+    if (
+      event.request.method === "PUT" &&
+      urlString.indexOf("is_favorite") > 0
+    ) {
+      // Must be updated a restaurant's favourite status
+      const urlDataPart = urlString.replace(restaurantApiUrl + "/", "");
+
+      const restId = Number(
+        urlDataPart.substring(0, urlDataPart.lastIndexOf("/"))
+      );
+      const favouriteStatus =
+        urlDataPart.substring(
+          urlDataPart.lastIndexOf("=") + 1,
+          urlDataPart.length
+        ) === "true";
+      event.respondWith(
+        fetch(event.request)
+          .then(resp => {
+            return getRestaurantFromCache(restId).then(restaurant => {
+              restaurant.is_favorite = favouriteStatus;
+              return updateRestaurantInCache(restaurant).then(() => {
+                return resp;
+              });
+            });
+          })
+          .catch(err => {
+            return getRestaurantFromCache(restId).then(restaurant => {
+              restaurant.is_favorite = favouriteStatus;
+              return updateRestaurantInCache(restaurant).then(() => {
+                return getRestaurantFromCache(restId).then(restaurant => {
+                  const responseJson = generateResponseFromJson(restaurant);
+                  return flagFavouriteAsOffline(restId, favouriteStatus).then(
+                    () => {
+                      return responseJson;
+                    }
+                  );
+                });
+              });
+            });
+          })
+      );
+    } else {
+      // Must be loading one or more restaurants
+      event.respondWith(
+        fetch(event.request)
+          .then(resp => {
+            const originalResp = resp.clone();
+            resp.json().then(rests => {
+              getStore().then(db => {
+                const tx = db.transaction(restaurantStore, "readwrite");
+                if (Array.isArray(rests)) {
+                  rests.map(rest => {
+                    tx.objectStore(restaurantStore).put(rest);
+                  });
+                } else {
+                  tx.objectStore(restaurantStore).put(rests);
+                }
+                return tx.complete;
+              });
+            });
+            return originalResp;
+          })
+          .catch(() => {
+            const isSingleRestaurant = Number.isInteger(
+              Number(urlString.substring(urlString.length - 1))
+            );
+
+            return getStore().then(db => {
+              const tx = db.transaction(restaurantStore, "readonly");
+              if (isSingleRestaurant) {
+                const restId = Number(
+                  urlString.substring(
+                    urlString.lastIndexOf("/") + 1,
+                    urlString.length
+                  )
+                );
+
+                return tx
+                  .objectStore(restaurantStore)
+                  .get(restId)
+                  .then(rest => {
+                    return generateResponseFromJson(rest);
+                  });
+              } else {
+                return tx
+                  .objectStore(restaurantStore)
+                  .getAll()
+                  .then(restaurants => {
+                    return generateResponseFromJson(restaurants);
+                  });
+              }
+            });
+          })
+      );
+    }
+  }
+
+  // Handle calls to reviews api
+  if (urlString.startsWith(reviewsApiUrl)) {
+    if (urlString === reviewsApiUrl && event.request.method === "POST") {
+      // Must be adding a new review to a restaurant
+      event.respondWith(
+        fetch(event.request.clone())
+          .then(resp => {
+            const originalResp = resp.clone();
+            resp.json().then(review => {
+              getStore().then(db => {
+                const tx = db.transaction(reviewStore, "readwrite");
+                tx.objectStore(reviewStore).put(review);
+                return tx.complete;
+              });
+            });
+            return originalResp;
+          })
+          .catch(err => {
+            return event.request.json().then(o => {
+              return getStore().then(db => {
+                const createdDate = new Date().toISOString();
+                o.createdAt = createdDate;
+                o.updatedAt = createdDate;
+                const tx = db.transaction(unsubmittedReviewStore, "readwrite");
+                tx.objectStore(unsubmittedReviewStore).put(o);
+                return tx.complete.then(() => {
+                  return generateResponseFromJson(o);
+                });
+              });
+            });
+          })
+      );
+    } else if (urlString.startsWith(`${reviewsApiUrl}/?restaurant_id=`)) {
+      // Must be loading reviews for a specific restaurant
+      const restId = Number(
+        urlString.replace(`${reviewsApiUrl}/?restaurant_id=`, "")
+      );
+      event.respondWith(
+        fetch(event.request)
+          .then(resp => {
+            const originalResp = resp.clone();
+            resp.json().then(reviews => {
+              getStore().then(db => {
+                const tx = db.transaction(reviewStore, "readwrite");
+                if (Array.isArray(reviews)) {
+                  reviews.map(review => {
+                    tx.objectStore(reviewStore).put(review);
+                  });
+                } else {
+                  tx.objectStore(reviewStore).put(reviews);
+                }
+                return tx.complete;
+              });
+            });
+            return originalResp;
+          })
+          .catch(() => {
+            return getReviewsByRestaurants(restId).then(results => {
+              return getUnsubmittedReviewsByRestaurants(restId).then(
+                subResults => {
+                  subResults.forEach(o => results.push(o));
+                  return generateResponseFromJson(results);
+                }
+              );
+            });
+          })
+      );
+    }
+  }
+
+  const url = new URL(urlString);
+  if (url.origin === location.origin) {
+    // Local resource, check to see if it exists in the cache
+    event.respondWith(
+      caches.open(internalCache).then(cache => {
+        //handle route path equiv
+        if (url.pathname === "/") {
+          return checkCacheAndRespond(cache, "/index.html");
+        }
+        //as datasource is cached, only cache page skeleton once
+        if (url.pathname.startsWith("/restaurant.html")) {
+          return checkCacheAndRespond(cache, "/restaurant.html");
+        }
+
+        return checkCacheAndRespond(cache, event.request);
+      })
+    );
+  }
+});
+
+self.addEventListener("message", event => {
+  if (event.data === "sync-offline") {
+    syncOfflineReviews().catch(err => {
+      console.error(
+        "There was a problem syncing offline reviews to the server",
+        err
+      );
+    });
+
+    syncOfflineFavourites().catch(err => {
+      console.error(
+        "There was a problem syncing favourite restaurants to the server",
+        err
+      );
+    });
+  }
+});
+
 initStore = () => {
   idb
     .open(storeName, storeVersion, upgradeDb => {
@@ -84,24 +303,6 @@ initStore = () => {
         });
     });
 };
-
-self.addEventListener("message", event => {
-  if (event.data === "sync-offline") {
-    syncOfflineReviews().catch(err => {
-      console.error(
-        "There was a problem syncing offline reviews to the server",
-        err
-      );
-    });
-
-    syncOfflineFavourites().catch(err => {
-      console.error(
-        "There was a problem syncing favourite restaurants to the server",
-        err
-      );
-    });
-  }
-});
 
 syncOfflineReviews = async () => {
   const unsubmittedReviews = await getAllUnsubmittedReviews();
@@ -258,202 +459,6 @@ generateResponseFromJson = json => {
     headers: { "Content-Type": "application/json" }
   });
 };
-
-self.addEventListener("install", event => {
-  event.waitUntil(cacheBaseAssets());
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(cleanOldCaches());
-  event.waitUntil(initStore());
-});
-
-self.addEventListener("fetch", event => {
-  const urlString = event.request.url;
-  const url = new URL(urlString);
-
-  if (urlString.startsWith(restaurantApiUrl)) {
-    if (
-      event.request.method === "PUT" &&
-      urlString.indexOf("is_favorite") > 0
-    ) {
-      const urlDataPart = urlString.replace(restaurantApiUrl + "/", "");
-
-      const restId = Number(
-        urlDataPart.substring(0, urlDataPart.lastIndexOf("/"))
-      );
-      const favouriteStatus =
-        urlDataPart.substring(
-          urlDataPart.lastIndexOf("=") + 1,
-          urlDataPart.length
-        ) === "true";
-      event.respondWith(
-        fetch(event.request)
-          .then(resp => {
-            return getRestaurantFromCache(restId).then(restaurant => {
-              restaurant.is_favorite = favouriteStatus;
-              return updateRestaurantInCache(restaurant).then(() => {
-                return resp;
-              });
-            });
-          })
-          .catch(err => {
-            return getRestaurantFromCache(restId).then(restaurant => {
-              restaurant.is_favorite = favouriteStatus;
-              return updateRestaurantInCache(restaurant).then(() => {
-                return getRestaurantFromCache(restId).then(restaurant => {
-                  const responseJson = generateResponseFromJson(restaurant);
-                  return flagFavouriteAsOffline(restId, favouriteStatus).then(
-                    () => {
-                      return responseJson;
-                    }
-                  );
-                });
-              });
-            });
-          })
-      );
-    } else {
-      event.respondWith(
-        fetch(event.request)
-          .then(resp => {
-            const originalResp = resp.clone();
-            resp.json().then(rests => {
-              getStore().then(db => {
-                const tx = db.transaction(restaurantStore, "readwrite");
-                if (Array.isArray(rests)) {
-                  rests.map(rest => {
-                    tx.objectStore(restaurantStore).put(rest);
-                  });
-                } else {
-                  tx.objectStore(restaurantStore).put(rests);
-                }
-                return tx.complete;
-              });
-            });
-            return originalResp;
-          })
-          .catch(() => {
-            const isSingleRestaurant = Number.isInteger(
-              Number(urlString.substring(urlString.length - 1))
-            );
-
-            return getStore().then(db => {
-              const tx = db.transaction(restaurantStore, "readonly");
-              if (isSingleRestaurant) {
-                const restId = Number(
-                  urlString.substring(
-                    urlString.lastIndexOf("/") + 1,
-                    urlString.length
-                  )
-                );
-
-                return tx
-                  .objectStore(restaurantStore)
-                  .get(restId)
-                  .then(rest => {
-                    return generateResponseFromJson(rest);
-                  });
-              } else {
-                return tx
-                  .objectStore(restaurantStore)
-                  .getAll()
-                  .then(restaurants => {
-                    return generateResponseFromJson(restaurants);
-                  });
-              }
-            });
-          })
-      );
-    }
-  }
-
-  if (urlString.startsWith(reviewsApiUrl)) {
-    if (urlString === reviewsApiUrl && event.request.method === "POST") {
-      event.respondWith(
-        fetch(event.request.clone())
-          .then(resp => {
-            const originalResp = resp.clone();
-            resp.json().then(review => {
-              getStore().then(db => {
-                const tx = db.transaction(reviewStore, "readwrite");
-                tx.objectStore(reviewStore).put(review);
-                return tx.complete;
-              });
-            });
-            return originalResp;
-          })
-          .catch(err => {
-            return event.request.json().then(o => {
-              return getStore().then(db => {
-                const createdDate = new Date().toISOString();
-                o.createdAt = createdDate;
-                o.updatedAt = createdDate;
-                const tx = db.transaction(unsubmittedReviewStore, "readwrite");
-                tx.objectStore(unsubmittedReviewStore).put(o);
-                return tx.complete.then(() => {
-                  return generateResponseFromJson(o);
-                });
-              });
-            });
-          })
-      );
-    }
-
-    if (urlString.startsWith(`${reviewsApiUrl}/?restaurant_id=`)) {
-      const restId = Number(
-        urlString.replace(`${reviewsApiUrl}/?restaurant_id=`, "")
-      );
-      event.respondWith(
-        fetch(event.request)
-          .then(resp => {
-            const originalResp = resp.clone();
-            resp.json().then(reviews => {
-              getStore().then(db => {
-                const tx = db.transaction(reviewStore, "readwrite");
-                if (Array.isArray(reviews)) {
-                  reviews.map(review => {
-                    tx.objectStore(reviewStore).put(review);
-                  });
-                } else {
-                  tx.objectStore(reviewStore).put(reviews);
-                }
-                return tx.complete;
-              });
-            });
-            return originalResp;
-          })
-          .catch(() => {
-            return getReviewsByRestaurants(restId).then(results => {
-              return getUnsubmittedReviewsByRestaurants(restId).then(
-                subResults => {
-                  subResults.forEach(o => results.push(o));
-                  return generateResponseFromJson(results);
-                }
-              );
-            });
-          })
-      );
-    }
-  }
-
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.open(internalCache).then(cache => {
-        //handle route path equiv
-        if (url.pathname === "/") {
-          return checkCacheAndRespond(cache, "/index.html");
-        }
-        //as datasource is cached, only cache page skeleton once
-        if (url.pathname.startsWith("/restaurant.html")) {
-          return checkCacheAndRespond(cache, "/restaurant.html");
-        }
-
-        return checkCacheAndRespond(cache, event.request);
-      })
-    );
-  }
-});
 
 getReviewsByRestaurants = async restId => {
   return getStore().then(db => {
